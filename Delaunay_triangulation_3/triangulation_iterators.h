898c53a1f170596f6236fc9fe1c57afb23a2d232
Sébastien Loriot <sloriot.ml@gmail.com>
2010-02-10 14:47:14 +0000

    factorize macro using a helper class
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/cgal-bindings/trunk@108 75acb805-9abe-4c10-b376-15f38c722ce8

--- Delaunay_triangulation_3/triangulation_iterators.h
+++ Delaunay_triangulation_3/triangulation_iterators.h
@@ -4,178 +4,105 @@
 #include "triangulation_handles.h"
 #include "triple.h"
 
+template <class T>
+struct Iterator_helper{
+  template <class Ti>
+  static T convert(const Ti& i){
+    return T(i);
+  }
+  
+  static T
+  default_value(){return T(NULL);}
+};
+
+template<class T1>
+struct Iterator_helper<std::pair<T1,int> >{
+  template <class T>
+  static std::pair<T1,int> convert(const T& i){
+    return std::pair<T1,int>(T1(i->first),i->second);
+  }
+  
+  static std::pair<T1,int>
+  default_value(){return std::pair<T1,int>(T1(NULL),-1);}
+};
+
+template<class T1>
+struct Iterator_helper<CGAL_SWIG::Triple<T1,int,int> >{
+  template <class T>
+  static CGAL_SWIG::Triple<T1,int,int> convert(const T& i){
+    return CGAL_SWIG::Triple<T1,int,int>(T1(i->first),i->second,i->third);
+  }
+  
+  static CGAL_SWIG::Triple<T1,int,int>
+  default_value(){return CGAL_SWIG::Triple<T1,int,int>(T1(NULL),-1,-1);}
+};
+
+template <>
+struct Iterator_helper<EPIC_Point_3>{
+  template <class Ti>
+  static EPIC_Point_3 convert(const Ti& i){
+    return EPIC_Point_3(*i);
+  }
+  
+  static EPIC_Point_3
+  default_value(){return EPIC_Point_3();}
+};
+
+
 #ifdef SWIGPYTHON
 #define DECLARE_TRIANGULATION_ITERATOR(NAME,RETURN)            \
 template<class Triangulation>                                  \
-class NAME{                                                    \
+class CGAL_##NAME{                                             \
   typename Triangulation::NAME cur;                            \
   typename Triangulation::NAME end;                            \
 public:                                                        \
                                                                \
-  NAME(                                    \
+  CGAL_##NAME(                                                 \
     typename Triangulation::NAME cur_,                         \
     typename Triangulation::NAME end_                          \
   ):cur(cur_),end(end_){}                                      \
                                                                \
-  NAME<Triangulation> __iter__(){return *this;}                \
-  RETURN next()                          \
+  CGAL_##NAME<Triangulation> __iter__(){return *this;}         \
+  RETURN next()                                                \
   {                                                            \
     if (cur!=end)                                              \
-      return RETURN ( (cur++) );         \
+      return Iterator_helper<RETURN>::convert ( (cur++) );     \
     throw 1;                                                   \
-    return RETURN(NULL);                 \
+    return Iterator_helper<RETURN>::default_value();           \
   }                                                            \
                                                                \
   bool hasNext(){                                              \
     return cur!=end;                                           \
   }                                                            \
 };       
-
-#define DECLARE_TRIANGULATION_ITERATOR_PAIR(NAME,FIRST,SECOND) \
-template<class Triangulation>                                  \
-class NAME{                                                    \
-  typename Triangulation::NAME cur;                            \
-  typename Triangulation::NAME end;                            \
-public:                                                        \
-                                                               \
-  NAME(                                                        \
-    typename Triangulation::NAME cur_,                         \
-    typename Triangulation::NAME end_                          \
-  ):cur(cur_),end(end_){}                                      \
-                                                               \
-  NAME __iter__(){return *this;}                               \
-  std::pair<FIRST,SECOND> next()                               \
-  {                                                            \
-    if (cur!=end){                                             \
-      typename Triangulation::NAME res=cur++;                  \
-      return                                                   \
-      std::pair<FIRST,SECOND> (FIRST(res->first),res->second); \
-    }                                                          \
-    throw 1;                                                   \
-    return std::pair<FIRST,SECOND> (FIRST(),0);                \
-  }                                                            \
-                                                               \
-  bool hasNext(){                                              \
-    return cur!=end;                                           \
-  }                                                            \
-};                                                             
-
-#define DECLARE_TRIANGULATION_ITERATOR_TRIPLE(NAME,FIRST,SECOND,THIRD) \
-template<class Triangulation>                                          \
-class NAME{                                                            \
-  typename Triangulation::NAME cur;                                    \
-  typename Triangulation::NAME end;                                    \
-public:                                                                \
-                                                                       \
-  NAME(                                                                \
-    typename Triangulation::NAME cur_,                                 \
-    typename Triangulation::NAME end_                                  \
-  ):cur(cur_),end(end_){}                                              \
-                                                                       \
-  NAME __iter__(){return *this;}                                       \
-  CGAL_SWIG::Triple<FIRST,SECOND,THIRD> next()                         \
-  {                                                                    \
-    if (cur!=end){                                                     \
-      typename Triangulation::NAME res=cur++;                          \
-      return                                                           \
-      CGAL_SWIG::Triple<FIRST,SECOND,THIRD> (FIRST(res->first),        \
-                                             res->second,              \
-                                             res->third);              \
-    }                                                                  \
-    throw 1;                                                           \
-    return CGAL_SWIG::Triple<FIRST,SECOND,THIRD>(FIRST(),-1,-1);       \
-  }                                                                    \
-                                                                       \
-  bool hasNext(){                                                      \
-    return cur!=end;                                                   \
-  }                                                                    \
-};
-
 #else
-
-
 #define DECLARE_TRIANGULATION_ITERATOR(NAME,RETURN)            \
 template<class Triangulation>                                  \
-class NAME{                                                    \
+class CGAL_##NAME{                                             \
   typename Triangulation::NAME cur;                            \
   typename Triangulation::NAME end;                            \
 public:                                                        \
                                                                \
-  NAME(                                                        \
+  CGAL_##NAME(                                                 \
     typename Triangulation::NAME cur_,                         \
     typename Triangulation::NAME end_                          \
   ):cur(cur_),end(end_){}                                      \
                                                                \
-  RETURN next()                          \
+  RETURN next()                                                \
   {                                                            \
     if (cur!=end)                                              \
-      return RETURN ( (cur++) );         \
-    return RETURN(NULL);                 \
+      return Iterator_helper<RETURN>::convert ( (cur++) );     \
+    return Iterator_helper<RETURN>::default_value();           \
   }                                                            \
                                                                \
   bool hasNext(){                                              \
     return cur!=end;                                           \
   }                                                            \
-};             
-
-#define DECLARE_TRIANGULATION_ITERATOR_PAIR(NAME,FIRST,SECOND) \
-template<class Triangulation>                                  \
-class NAME{                                                    \
-  typename Triangulation::NAME cur;                            \
-  typename Triangulation::NAME end;                            \
-public:                                                        \
-                                                               \
-  NAME(                                                        \
-    typename Triangulation::NAME cur_,                         \
-    typename Triangulation::NAME end_                          \
-  ):cur(cur_),end(end_){}                                      \
-                                                               \
-  std::pair<FIRST,SECOND> next()                               \
-  {                                                            \
-    if (cur!=end){                                             \
-      typename Triangulation::NAME res=cur++;                  \
-      return                                                   \
-      std::pair<FIRST,SECOND> (FIRST(res->first),res->second); \
-    }                                                          \
-    return std::pair<FIRST,SECOND> (FIRST(),0);                \
-  }                                                            \
-                                                               \
-  bool hasNext(){                                              \
-    return cur!=end;                                           \
-  }                                                            \
-};                                                             
-
-#define DECLARE_TRIANGULATION_ITERATOR_TRIPLE(NAME,FIRST,SECOND,THIRD) \
-template<class Triangulation>                                          \
-class NAME{                                                            \
-  typename Triangulation::NAME cur;                                    \
-  typename Triangulation::NAME end;                                    \
-public:                                                                \
-                                                                       \
-  NAME(                                                                \
-    typename Triangulation::NAME cur_,                                 \
-    typename Triangulation::NAME end_                                  \
-  ):cur(cur_),end(end_){}                                              \
-                                                                       \
-  CGAL_SWIG::Triple<FIRST,SECOND,THIRD> next()                         \
-  {                                                                    \
-    if (cur!=end){                                                     \
-      typename Triangulation::NAME res=cur++;                          \
-      return                                                           \
-      CGAL_SWIG::Triple<FIRST,SECOND,THIRD> (FIRST(res->first),        \
-                                             res->second,              \
-                                             res->third);              \
-    }                                                                  \
-    return CGAL_SWIG::Triple<FIRST,SECOND,THIRD>(FIRST(),-1,-1);       \
-  }                                                                    \
-                                                                       \
-  bool hasNext(){                                                      \
-    return cur!=end;                                                   \
-  }                                                                    \
 };
-
 #endif
 
+#define PAIR_CHI std::pair<CGAL_Cell_handle<Triangulation>,int>
+#define TRIPLE_CHII CGAL_SWIG::Triple<CGAL_Cell_handle<Triangulation>,int,int>
 
 DECLARE_TRIANGULATION_ITERATOR(All_vertices_iterator,CGAL_Vertex_handle<Triangulation>)
 DECLARE_TRIANGULATION_ITERATOR(Finite_vertices_iterator,CGAL_Vertex_handle<Triangulation>)
@@ -183,47 +110,21 @@ DECLARE_TRIANGULATION_ITERATOR(Finite_vertices_iterator,CGAL_Vertex_handle<Trian
 DECLARE_TRIANGULATION_ITERATOR(All_cells_iterator,CGAL_Cell_handle<Triangulation>)
 DECLARE_TRIANGULATION_ITERATOR(Finite_cells_iterator,CGAL_Cell_handle<Triangulation>)
 
-DECLARE_TRIANGULATION_ITERATOR_PAIR(All_facets_iterator,CGAL_Cell_handle<Triangulation> , int)
-DECLARE_TRIANGULATION_ITERATOR_PAIR(Finite_facets_iterator,CGAL_Cell_handle<Triangulation> , int)
+DECLARE_TRIANGULATION_ITERATOR(All_facets_iterator,PAIR_CHI)
+DECLARE_TRIANGULATION_ITERATOR(Finite_facets_iterator,PAIR_CHI)
 
-DECLARE_TRIANGULATION_ITERATOR_TRIPLE(All_edges_iterator,CGAL_Cell_handle<Triangulation> , int, int)
-DECLARE_TRIANGULATION_ITERATOR_TRIPLE(Finite_edges_iterator,CGAL_Cell_handle<Triangulation> , int, int)
+DECLARE_TRIANGULATION_ITERATOR(All_edges_iterator,TRIPLE_CHII)
+DECLARE_TRIANGULATION_ITERATOR(Finite_edges_iterator,TRIPLE_CHII)
 
-template<class Triangulation>
-class Point_iterator{
-  typename Triangulation::Point_iterator cur;
-  typename Triangulation::Point_iterator end;
-public:
-
-  Point_iterator(
-    typename Triangulation::Point_iterator cur_,
-    typename Triangulation::Point_iterator end_
-  ):cur(cur_),end(end_){}
-  #ifdef SWIGPYTHON
-  Point_iterator<Triangulation> __iter__(){return *this;}
-  #endif
-  EPIC_Point_3 next()
-  {
-    if (cur!=end)
-      return EPIC_Point_3 ( *(cur++) );
-    #ifdef SWIGPYTHON
-    throw 1;
-    #endif
-    return EPIC_Point_3();
-  }
-
-  bool hasNext(){
-    return cur!=end;
-  }
-};
+DECLARE_TRIANGULATION_ITERATOR(Point_iterator,EPIC_Point_3)
 
 template<class Triangulation>
-class Cell_circulator{
+class CGAL_Cell_circulator{
   typename Triangulation::Cell_circulator cur;
 public:
-  Cell_circulator( typename Triangulation::Cell_circulator cur_):cur(cur_){}
+  CGAL_Cell_circulator( typename Triangulation::Cell_circulator cur_):cur(cur_){}
   #ifdef SWIGPYTHON
-  Cell_circulator<Triangulation> __iter__(){return *this;}
+  CGAL_Cell_circulator<Triangulation> __iter__(){return *this;}
   #endif
   CGAL_Cell_handle<Triangulation> next() { return CGAL_Cell_handle<Triangulation> ( (cur++) ); }
 
@@ -231,12 +132,12 @@ public:
 };
 
 template<class Triangulation>
-class Facet_circulator{
+class CGAL_Facet_circulator{
   typename Triangulation::Facet_circulator cur;
 public:
-  Facet_circulator( typename Triangulation::Facet_circulator cur_):cur(cur_){}
+  CGAL_Facet_circulator( typename Triangulation::Facet_circulator cur_):cur(cur_){}
   #ifdef SWIGPYTHON
-  Facet_circulator<Triangulation> __iter__(){return *this;}
+  CGAL_Facet_circulator<Triangulation> __iter__(){return *this;}
   #endif
   std::pair<CGAL_Cell_handle<Triangulation>,int> next(){
     typename Triangulation::Facet_circulator res=cur++;
