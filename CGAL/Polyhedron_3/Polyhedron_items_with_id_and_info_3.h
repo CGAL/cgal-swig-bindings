9767df5104378728c5349ce79d91f73673999385
Sébastien Loriot <sloriot.ml@gmail.com>
2010-04-23 14:35:39 +0000

    work in progress: jobject stored inside halfedge wroking. Need to clean up
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/cgal-bindings/trunk@149 75acb805-9abe-4c10-b376-15f38c722ce8

---
+++ CGAL/Polyhedron_3/Polyhedron_items_with_id_and_info_3.h
@@ -0,0 +1,89 @@
+#ifndef CGAL_SWIG_POLYHEDRON_3_POLYHEDRON_ITEMS_WITH_ID_AND_INFO_3_H
+#define CGAL_SWIG_POLYHEDRON_3_POLYHEDRON_ITEMS_WITH_ID_AND_INFO_3_H
+
+
+#include <CGAL/HalfedgeDS_vertex_base.h>
+#include <CGAL/HalfedgeDS_halfedge_base.h>
+#include <CGAL/HalfedgeDS_face_base.h>
+
+namespace CGAL{
+
+template < class Refs, class P, class ID, class Info>
+class HalfedgeDS_vertex_max_base_with_id_and_info : public HalfedgeDS_vertex_base< Refs, Tag_true, P>
+{
+public:
+    typedef HalfedgeDS_vertex_base< Refs, Tag_true, P> Base ;
+    typedef ID size_type ;
+    typedef P Point ;
+private:
+    size_type mID ;
+    Info info_;
+public:
+    HalfedgeDS_vertex_max_base_with_id_and_info() : mID ( size_type(-1) )  {}
+    HalfedgeDS_vertex_max_base_with_id_and_info( Point const& p) : Base(p), mID ( size_type(-1) ) {}
+    HalfedgeDS_vertex_max_base_with_id_and_info( Point const& p, size_type i ) : Base(p), mID(i) {}
+    size_type&       id()       { return mID; }
+    size_type const& id() const { return mID; }
+    Info& info(){return info_;}
+};
+
+
+template < class Refs, class ID, class Info>
+class HalfedgeDS_halfedge_max_base_with_id_and_info : public HalfedgeDS_halfedge_base< Refs, Tag_true, Tag_true, Tag_true >
+{
+public:
+    typedef HalfedgeDS_halfedge_base< Refs, Tag_true, Tag_true, Tag_true> Base ;
+    typedef typename Base::Base_base Base_base ;
+    typedef ID size_type ;
+private:
+    size_type mID ;
+    Info info_;
+public:
+    HalfedgeDS_halfedge_max_base_with_id_and_info( size_type i = size_type(-1) ) : mID(i) {}
+    size_type&       id()       { return mID; }
+    size_type const& id() const { return mID; }
+    Info& info(){return info_;}
+};
+
+template < class Refs, class Pln, class ID, class Info>
+class HalfedgeDS_face_max_base_with_id_and_info : public HalfedgeDS_face_base< Refs, Tag_true, Pln>
+{
+public:
+    typedef HalfedgeDS_face_base< Refs, Tag_true, Pln> Base ;
+    typedef ID size_type ;
+private:
+    size_type mID ;
+    Info info_;
+public:
+    HalfedgeDS_face_max_base_with_id_and_info() : mID ( size_type(-1) ) {}
+    HalfedgeDS_face_max_base_with_id_and_info( Pln const& p) : Base(p), mID ( size_type(-1) ) {}
+    HalfedgeDS_face_max_base_with_id_and_info( Pln const& p, size_type i ) : Base(p), mID (i) {}
+    size_type&       id()       { return mID; }
+    size_type const& id() const { return mID; }
+    Info& info(){return info_;}
+};
+
+
+template <class Info>
+class Polyhedron_items_with_id_and_info_3 {
+public:
+    template < class Refs, class Traits>
+    struct Vertex_wrapper {
+        typedef typename Traits::Point_3 Point;
+        typedef HalfedgeDS_vertex_max_base_with_id_and_info< Refs, Point, std::size_t,Info> Vertex;
+    };
+    template < class Refs, class Traits>
+    struct Halfedge_wrapper {
+        typedef HalfedgeDS_halfedge_max_base_with_id_and_info<Refs, std::size_t,Info> Halfedge;
+    };
+    template < class Refs, class Traits>
+    struct Face_wrapper {
+        typedef HalfedgeDS_face_max_base_with_id_and_info< Refs, Tag_false, std::size_t,Info>  Face;
+    };
+};
+
+
+}//namespace CGAL
+
+#endif //CGAL_SWIG_POLYHEDRON_3_POLYHEDRON_ITEMS_WITH_ID_AND_INFO_3_H
+
--- CGAL/Polyhedron_3/config.i
+++ CGAL/Polyhedron_3/config.i
@@ -1,13 +1,23 @@
-%{#define CGAL_SWIG_FACET_SUPPORTS_PLANE%}
-#define CGAL_SWIG_FACET_SUPPORTS_PLANE
+//~ %{#define CGAL_SWIG_FACET_SUPPORTS_PLANE%}
+//~ #define CGAL_SWIG_FACET_SUPPORTS_PLANE
 
-%define Polyhedron_3_
-CGAL::Polyhedron_3<EPIC_Kernel>
-%enddef
+//~ %define Polyhedron_3_
+//~ CGAL::Polyhedron_3<EPIC_Kernel>
+//~ %enddef
 
 // %define Polyhedron_3_
 // CGAL::Polyhedron_3<EPIC_Kernel,CGAL::Polyhedron_items_with_id_3>
 // %enddef
+
+%include "../Java/JavaData.h"
+%{
+#include "../Java/JavaData.h"
+#include "Polyhedron_items_with_id_and_info_3.h"
+%}
+
+%define Polyhedron_3_
+CGAL::Polyhedron_3<EPIC_Kernel,CGAL::Polyhedron_items_with_id_and_info_3<JavaData> >
+%enddef
 
 %define Halfedge_handle_
 CGAL_Halfedge_handle<Polyhedron_3_> 
@@ -26,3 +36,8 @@ CGAL_Facet_handle<Polyhedron_3_>
 %template(Polyhedron_3_Modifier_1)                     Modifier_wrapper< Polyhedron_3_,Build_triangle<Polyhedron_3_::HalfedgeDS> >;
 %template(Polyhedron_3_Modifier_2)                     Modifier_wrapper< Polyhedron_3_,Build_square<Polyhedron_3_::HalfedgeDS> >;
 
+%extend Halfedge_handle_ {
+  JavaData& info(){
+    return $self->get_data_ref()->info();
+  }
+}
