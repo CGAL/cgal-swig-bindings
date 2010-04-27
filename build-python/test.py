b97d87b038776ce064d91d7a430485c8ef62d88d
Sébastien Loriot <sloriot.ml@gmail.com>
2010-04-27 12:44:08 +0000

    update python example
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/cgal-bindings/trunk@153 75acb805-9abe-4c10-b376-15f38c722ce8

--- build-python/test.py
+++ build-python/test.py
@@ -1,11 +1,11 @@
-from CGAL import Kernel
-from CGAL import Triangulation_3
-t=Triangulation_3 .DT3_T()
+from CGAL import CGAL_Kernel
+from CGAL import CGAL_Triangulation_3
+t=CGAL_Triangulation_3 .Delaunay_triangulation_3()
 l=[]
-l.append(Kernel.Point_3(1,1,1))
-l.append(Kernel.Point_3(2,2,2))
-l.append(Kernel.Point_3(441,41,84))
-l.append(Kernel.Point_3(1,1,8))
+l.append(CGAL_Kernel.Point_3(1,1,1))
+l.append(CGAL_Kernel.Point_3(2,2,2))
+l.append(CGAL_Kernel.Point_3(441,41,84))
+l.append(CGAL_Kernel.Point_3(1,1,8))
 t.insert_range(l)
 
 for p in t.finite_vertices():
