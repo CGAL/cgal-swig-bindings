6146756e936e635723672aeb6a4f73383a561828
Stéphane Noyer <noyer.stephane@gmail.com>
2010-04-30 13:25:52 +0000

    added hashCode functions on Polyhedron_3 items handles
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/cgal-bindings/trunk@172 75acb805-9abe-4c10-b376-15f38c722ce8

--- CGAL/Polyhedron_3/polyhedron_3_handles.h
+++ CGAL/Polyhedron_3/polyhedron_3_handles.h
@@ -81,6 +81,7 @@ public:
   FORWARD_CALL_0_PTR(CGAL_Facet_handle<Polyhedron_base>,facet)
   
   bool equals(const CGAL_Halfedge_handle<Polyhedron_base>& he){ return get_data()==he.get_data(); }
+  int hashCode(){ return *reinterpret_cast<int*> (&*data);}
 
   int id(){return internal::Id<typename Polyhedron_base::Items>::get(data);}
   void set_id(int i){internal::Id<typename Polyhedron_base::Items>::set(data,i);}
@@ -109,6 +110,7 @@ public:
   FORWARD_CALL_0_PTR(bool,is_trivalent)
 
   bool equals(const CGAL_Vertex_handle<Polyhedron_base>& v){ return get_data()==v.get_data(); }
+  int hashCode(){ return *reinterpret_cast<int*> (&*data);}
 
   int id(){return internal::Id<typename Polyhedron_base::Items>::get(data);}
   void set_id(int i){internal::Id<typename Polyhedron_base::Items>::set(data,i);}  
@@ -141,6 +143,7 @@ public:
   FORWARD_CALL_0_PTR(bool,is_quad)
 
   bool equals(const CGAL_Facet_handle<Polyhedron_base>& f){ return get_data()==f.get_data(); }
+  int hashCode(){ return *reinterpret_cast<int*> (&*data);}
 
   int id(){return internal::Id<typename Polyhedron_base::Items>::get(data);}
   void set_id(int i){internal::Id<typename Polyhedron_base::Items>::set(data,i);}  
