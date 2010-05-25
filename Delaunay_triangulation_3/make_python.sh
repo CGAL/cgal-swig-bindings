33041c53b2ea2ea26c140fed97f3003800f8dc55
Sébastien Loriot <sloriot.ml@gmail.com>
2010-05-25 08:17:55 +0000

    move my tests to branches
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/cgal-bindings/trunk@180 75acb805-9abe-4c10-b376-15f38c722ce8

--- Delaunay_triangulation_3/make_python.sh
+++
@@ -1,6 +0,0 @@
-swig -python -c++ DT3.i
-g++ -fPIC -frounding-math  -c  DT3_wrap.cxx  -I/usr/include/python2.5 -I${CGAL_DIR}/../../../include -I${CGAL_DIR}/include
-g++ -fPIC -frounding-math  -DSWIGPYTHON -c  EPIC_Kernel.cpp  -I/usr/include/python2.5 -I${CGAL_DIR}/../../../include -I${CGAL_DIR}/include
-g++ -shared DT3_wrap.o EPIC_Kernel.o -o _DT3.so -lCGAL -lm -L${CGAL_DIR}/lib
-
-#python main.py
