e7310f0932eaedf51a7d1e83ba0e3985a437bbc1
Sébastien Loriot <sloriot.ml@gmail.com>
2010-06-09 09:04:46 +0000

    update test
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/cgal-bindings/trunk@194 75acb805-9abe-4c10-b376-15f38c722ce8

--- build-python/test_t2.py
+++ build-python/test_t2.py
@@ -1,6 +1,6 @@
 from CGAL import CGAL_Kernel
 from CGAL import CGAL_Triangulation_2
-t=CGAL_Triangulation_2.Triangulation_2()
+t=CGAL_Triangulation_2.Constrained_Delaunay_triangulation_plus_2()
 v1=t.insert(CGAL_Kernel.Point_2(1,2))
 v2=t.insert(CGAL_Kernel.Point_2(3,2))
 v3=t.insert(CGAL_Kernel.Point_2(3,44))
