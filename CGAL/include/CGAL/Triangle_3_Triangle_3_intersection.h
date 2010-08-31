f89a57acc38b1f64f713b382b753eb8015c9a194
Sébastien Loriot <sloriot.ml@gmail.com>
2010-08-31 13:43:54 +0000

    add missing intersection primitives
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/cgal-bindings/trunk@243 75acb805-9abe-4c10-b376-15f38c722ce8

--- CGAL/include/CGAL/Triangle_3_Triangle_3_intersection.h
+++ CGAL/include/CGAL/Triangle_3_Triangle_3_intersection.h
@@ -1,10 +1,85 @@
+// Copyright (c) 2010 GeometryFactory (France).
+// All rights reserved.
+//
+// This file is part of CGAL (www.cgal.org); you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public License as
+// published by the Free Software Foundation; version 2.1 of the License.
+// See the file LICENSE.LGPL distributed with CGAL.
+//
+// Licensees holding a valid commercial license may use this file in
+// accordance with the commercial license agreement provided with the software.
+//
+// This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+// WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+//
+// $URL$
+// $Id$
+//
+//
+// Author(s)     : Sebastien Loriot
+//
+
 #ifndef CGAL_TRIANGLE_3_TRIANGLE_3_INTERSECTION_H
 #define CGAL_TRIANGLE_3_TRIANGLE_3_INTERSECTION_H
+
+#include <boost/next_prior.hpp>
+#include <list>
+#include <vector>
+#include <map>
 
 namespace CGAL {
 
 namespace internal{
 
+template <class Kernel>
+void intersection_coplanar_triangles_cutoff(
+  const typename Kernel::Point_3& p,
+  const typename Kernel::Point_3& q,
+  const typename Kernel::Point_3& r,
+  const Kernel& k,
+  std::list<typename Kernel::Point_3>& inter_pts
+)
+{
+  if ( inter_pts.empty() ) return;
+  typedef typename std::list<typename Kernel::Point_3>::iterator Iterator;
+  typename Kernel::Coplanar_orientation_3 orient=k.coplanar_orientation_3_object();
+  typename Kernel::Construct_line_3 Line_3=k.construct_line_3_object();
+  //orient(p,q,r,r) is POSITIVE
+  std::map<const typename Kernel::Point_3*,Orientation> orientations;
+  for (Iterator it=inter_pts.begin();it!=inter_pts.end();++it)
+    orientations[ &(*it) ]=orient(p,q,r,*it);
+
+  int pt_added=0;
+  
+  const typename Kernel::Point_3* prev = &(*boost::prior(inter_pts.end()));
+  Iterator stop = inter_pts.size() > 2 ? inter_pts.end() : boost::prior(inter_pts.end());
+  for (Iterator it=inter_pts.begin();it!=stop;++it)
+  {
+    const typename Kernel::Point_3& curr=*it;
+    Orientation or_prev=orientations[prev],or_curr=orientations[&curr];
+    if ( (or_prev==POSITIVE && or_curr==NEGATIVE) || (or_prev==NEGATIVE && or_curr==POSITIVE) )
+    {
+      Object obj= intersection(Line_3(p,q),Line_3(*prev,curr),k);
+      const typename Kernel::Point_3* inter=object_cast<typename Kernel::Point_3>(&obj);
+      CGAL_kernel_assertion(inter!=NULL);
+      prev=&(* inter_pts.insert(it,*inter) );
+      orientations[prev]=COLLINEAR;
+      ++pt_added;
+    }
+    prev=&(*it);    
+  }
+  
+  CGAL_kernel_assertion(pt_added<3);
+  Iterator it=inter_pts.begin();
+  while(it!=inter_pts.end())
+  {
+    if (orientations[&(*it)]==NEGATIVE)
+      inter_pts.erase(it++);
+    else
+      ++it;
+  }
+}
+  
 template <class Kernel>
 Object
 intersection_coplanar_triangles(
@@ -13,8 +88,33 @@ intersection_coplanar_triangles(
   const Kernel& k)
 {
   typename Kernel::Has_on_3 has_on=k.has_on_3_object();
+  const typename Kernel::Point_3& p=t1.vertex(0),q=t1.vertex(1),r=t1.vertex(2);
   
-  return Object();
+  std::list<typename Kernel::Point_3> inter_pts;
+  inter_pts.push_back(t2.vertex(0));
+  inter_pts.push_back(t2.vertex(1));
+  inter_pts.push_back(t2.vertex(2));
+
+  //intersect t2 with the three half planes which intersection defines t1
+  intersection_coplanar_triangles_cutoff(p,q,r,k,inter_pts); //line pq
+  intersection_coplanar_triangles_cutoff(q,r,p,k,inter_pts); //line qr
+  intersection_coplanar_triangles_cutoff(r,p,q,k,inter_pts); //line rp
+  
+  switch ( inter_pts.size() ) {
+    case 0:
+      return Object();
+    case 1:
+      return make_object( * inter_pts.begin() );
+    case 2:
+      return make_object( k.construct_segment_3_object()(*inter_pts.begin(),
+                                                         *boost::next(inter_pts.begin())) );
+    case 3:
+      return make_object( k.construct_triangle_3_object()(*inter_pts.begin(),
+                                                          *boost::next(inter_pts.begin()),
+                                                          *boost::prior(inter_pts.end())) );
+    default:
+      return make_object( std::vector<typename Kernel::Point_3>(inter_pts.begin(),inter_pts.end()) );
+  }
 }
   
 template <class Kernel>
