68ded290eb08fd565f0c2543ec685c319d1e9309
Sébastien Loriot <sloriot.ml@gmail.com>
2010-10-14 14:25:42 +0000

    clean up and put into the constructor jclass and JMethod_id to avoid recomputing them
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/cgal-bindings/trunk@270 75acb805-9abe-4c10-b376-15f38c722ce8

--- SWIG_CGAL/Java/Java_caller_code.h
+++ SWIG_CGAL/Java/Java_caller_code.h
@@ -9,54 +9,69 @@
 template <class Input_wrapper,class Output_wrapper>
 class Java_caller_code
 {
-  jobject obj;
-  jclass obj_class;
-  jmethodID method_id;
-  //jmethodID static_method_id;
+  //info for calling the java predicate
+  jobject java_predicate;
+  jclass predicate_class;
+  jmethodID predicate_id;
+  // types of input and output in java format
+  std::string input_type_,output_type_;
+  //info for input
+  jclass input_class;
+  jmethodID input_cst_id;
+  //info for output
+  jclass output_class;
+  jmethodID get_output_id;
+  
 public:
 
-  Java_caller_code(jobject jobj,const char* fname, const char* fsign)
+  Java_caller_code(jobject jobj,const char* fname, const char* input_type,const char* output_type)
+  :input_type_(input_type),output_type_(output_type)
   {
-    obj=JNU_GetEnv()->NewGlobalRef(jobj);
-    obj_class=JNU_GetEnv()->GetObjectClass(obj);
-    assert(obj_class!=NULL);
-    method_id=JNU_GetEnv()->GetMethodID(obj_class, fname, fsign);
-    assert(method_id!=NULL);
+    //set info for java predicate
+    std::string fsign=std::string("(")+input_type_+std::string(")")+output_type_;
+    java_predicate=JNU_GetEnv()->NewGlobalRef(jobj);
+    predicate_class=JNU_GetEnv()->GetObjectClass(java_predicate);
+    assert(predicate_class!=NULL);
+    predicate_id=JNU_GetEnv()->GetMethodID(predicate_class, fname, fsign.c_str());
+    assert(predicate_id!=NULL);
+    //set info for input
+    input_class=JNU_GetEnv()->FindClass(input_type_.c_str());
+    input_cst_id=JNU_GetEnv()->GetMethodID(input_class,"<init>", "(JZ)V");
+    //set info for output
+    output_class=JNU_GetEnv()->FindClass(output_type_.c_str());
+    std::string output_signature=std::string("(")+output_type_+std::string(")J");
+    get_output_id=JNU_GetEnv()->GetStaticMethodID(output_class, "getCPtr",output_signature.c_str());
   }
   
   ~Java_caller_code()
   {
-    JNU_GetEnv()->DeleteGlobalRef(obj);
+    JNU_GetEnv()->DeleteGlobalRef(java_predicate);
   }
   
   
   #ifndef SWIG
   void run0(jobject data)
-  {JNU_GetEnv()->CallVoidMethod(obj,method_id,data);}
+  {JNU_GetEnv()->CallVoidMethod(java_predicate,predicate_id,data);}
   
   jobject run(jobject data)
-  {return JNU_GetEnv()->CallObjectMethod(obj,method_id,data);}
+  {return JNU_GetEnv()->CallObjectMethod(java_predicate,predicate_id,data);}
   
-  typename Output_wrapper::cpp_base run(const typename Input_wrapper::cpp_base& input,std::string input_signature,std::string output_signature) const
+  typename Output_wrapper::cpp_base run(const typename Input_wrapper::cpp_base& input) const
   {
     //create the wrapper object
     Input_wrapper*  cpp_wrapper=new Input_wrapper(input);
     //create the java corresponding object
-    jclass obj_class = JNU_GetEnv()->FindClass(input_signature.c_str());
-    assert( obj_class !=NULL );
-    jmethodID init_id = JNU_GetEnv()->GetMethodID(obj_class,"<init>", "(JZ)V");    
-    jlong hptr = (jlong) cpp_wrapper;
-    jobject ho = JNU_GetEnv()->NewObject(obj_class, init_id, hptr,true);
-    assert(ho!=NULL);
+    assert( input_class !=NULL );
+    assert( input_cst_id !=NULL );
+    jobject input_in_java = JNU_GetEnv()->NewObject(input_class,input_cst_id, (jlong) cpp_wrapper,true);
+    assert(input_in_java!=NULL);
+    
     //call java method
-    jobject res = JNU_GetEnv()->CallObjectMethod(obj,method_id,ho);
+    jobject res = JNU_GetEnv()->CallObjectMethod(java_predicate,predicate_id,input_in_java);
     //convert the result back to c++
-    jclass res_class=JNU_GetEnv()->GetObjectClass(res);
-    assert(res_class!=NULL);
-    output_signature=std::string("(")+output_signature+std::string(")J");
-    jmethodID getCPtr_id=JNU_GetEnv()->GetStaticMethodID(res_class, "getCPtr",output_signature.c_str());
-    assert(getCPtr_id!=NULL);
-    jlong jcpp=(jlong) JNU_GetEnv()->CallStaticObjectMethod(res_class,getCPtr_id,res);
+    assert(output_class!=NULL);
+    assert(get_output_id!=NULL);
+    jlong jcpp=(jlong) JNU_GetEnv()->CallStaticObjectMethod(output_class,get_output_id,res);
     assert( (void*) jcpp != NULL );
     return reinterpret_cast<Output_wrapper*>(jcpp)->get_data();
   }
