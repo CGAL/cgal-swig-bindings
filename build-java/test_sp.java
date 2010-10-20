02a1224edba5cad908eba15767e00901c2e16cbe
Sébastien Loriot <sloriot.ml@gmail.com>
2010-10-20 15:06:55 +0000

    add spatial searching 3d test
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/cgal-bindings/trunk@286 75acb805-9abe-4c10-b376-15f38c722ce8

--- build-java/test_sp.java
+++ build-java/test_sp.java
@@ -1,18 +1,21 @@
 import CGAL.Kernel.Point_2;
+import CGAL.Kernel.Point_3;
 import CGAL.Spatial_searching.Orthogonal_incremental_neighbor_search_tree_2;
 import CGAL.Spatial_searching.Orthogonal_incremental_neighbor_search_2;
+import CGAL.Spatial_searching.Orthogonal_incremental_neighbor_search_tree_3;
+import CGAL.Spatial_searching.Orthogonal_incremental_neighbor_search_3;
 import CGAL.Spatial_searching.K_neighbor_search_tree_2;
 import CGAL.Spatial_searching.K_neighbor_search_2;
 import CGAL.Spatial_searching.Point_with_transformed_distance_2;
+import CGAL.Spatial_searching.Point_with_transformed_distance_3;
 import CGAL.Spatial_searching.Fuzzy_sphere_2;
 import CGAL.Spatial_searching.Fuzzy_iso_box_2;
 import java.util.LinkedList;
 
 public class test_sp {
-  public static void main(String arg[]){
-    System.loadLibrary("CGAL_Spatial_searching");
-    
-    
+  
+  public static void test_2d()
+  {
     LinkedList<Point_2> lst=new LinkedList<Point_2>();
     lst.add( new Point_2(0,0) );
     lst.add( new Point_2(0,4) );
@@ -42,7 +45,35 @@ public class test_sp {
     tree2.insert_range(lst.iterator());
     K_neighbor_search_2 search2 = new K_neighbor_search_2(tree2,new Point_2(0,0),3);
     for (Point_with_transformed_distance_2 p : search2.iterator())
+      System.out.println(p.getFirst()+" "+p.getSecond());        
+  }
+  
+  public static void test_3d()
+  {
+    
+    LinkedList<Point_3> lst=new LinkedList<Point_3>();
+    lst.add( new Point_3(0,0,1) );
+    lst.add( new Point_3(0,4,2) );
+    lst.add( new Point_3(44,0,3) );
+    lst.add( new Point_3(44,5,4) );
+    lst.add( new Point_3(444,51,5) );
+    lst.add( new Point_3(14,1,7) );    
+    
+    Orthogonal_incremental_neighbor_search_tree_3 tree=new Orthogonal_incremental_neighbor_search_tree_3(lst.iterator());
+    
+    Orthogonal_incremental_neighbor_search_3 search = new Orthogonal_incremental_neighbor_search_3(tree,new Point_3(0,0,0));
+    
+    for (Point_with_transformed_distance_3 p : search.iterator())
       System.out.println(p.getFirst()+" "+p.getSecond());    
+    
+  }
+  
+  public static void main(String arg[]){
+    System.loadLibrary("CGAL_Spatial_searching");
+    test_2d();
+    test_3d();
+    
+
   }
   
 }
