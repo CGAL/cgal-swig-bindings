6d790fcc330077f86bb44c8d6cb50ba0169ec70c
Sébastien Loriot <sloriot.ml@gmail.com>
2010-10-29 13:26:26 +0000

    remove warning
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/cgal-bindings/trunk@303 75acb805-9abe-4c10-b376-15f38c722ce8

--- SWIG_CGAL/Kernel/global_function_signatures.h
+++ SWIG_CGAL/Kernel/global_function_signatures.h
@@ -1,4 +1,3 @@
-#warning UPDATE MISSING CPP FUNCTIONS
 //squared_distance
 //dimension 3
 FORWARD_CALL_GF_2(double,squared_distance,Point_3,Point_3)
@@ -73,13 +72,13 @@ FORWARD_CALL_GF_2(bool,do_intersect,Segment_3,Plane_3)
 FORWARD_CALL_GF_2(bool,do_intersect,Segment_3,Segment_3)
 FORWARD_CALL_GF_2(bool,do_intersect,Segment_3,Line_3)
 FORWARD_CALL_GF_2(bool,do_intersect,Segment_3,Triangle_3)
-//FORWARD_CALL_GF_2(bool,do_intersect,Segment_3,Ray_3)
+FORWARD_CALL_GF_2(bool,do_intersect,Segment_3,Ray_3)
 
 FORWARD_CALL_GF_2(bool,do_intersect,Line_3,Plane_3)
 FORWARD_CALL_GF_2(bool,do_intersect,Line_3,Segment_3)
 FORWARD_CALL_GF_2(bool,do_intersect,Line_3,Line_3)
 FORWARD_CALL_GF_2(bool,do_intersect,Line_3,Triangle_3)
-//FORWARD_CALL_GF_2(bool,do_intersect,Line_3,Ray_3)
+FORWARD_CALL_GF_2(bool,do_intersect,Line_3,Ray_3)
 
 FORWARD_CALL_GF_2(bool,do_intersect,Triangle_3,Plane_3)
 FORWARD_CALL_GF_2(bool,do_intersect,Triangle_3,Segment_3)
@@ -88,10 +87,10 @@ FORWARD_CALL_GF_2(bool,do_intersect,Triangle_3,Triangle_3)
 FORWARD_CALL_GF_2(bool,do_intersect,Triangle_3,Ray_3)
 
 FORWARD_CALL_GF_2(bool,do_intersect,Ray_3,Plane_3)
-//FORWARD_CALL_GF_2(bool,do_intersect,Ray_3,Segment_3)
-//FORWARD_CALL_GF_2(bool,do_intersect,Ray_3,Line_3)
+FORWARD_CALL_GF_2(bool,do_intersect,Ray_3,Segment_3)
+FORWARD_CALL_GF_2(bool,do_intersect,Ray_3,Line_3)
 FORWARD_CALL_GF_2(bool,do_intersect,Ray_3,Triangle_3)
-//FORWARD_CALL_GF_2(bool,do_intersect,Ray_3,Ray_3)
+FORWARD_CALL_GF_2(bool,do_intersect,Ray_3,Ray_3)
 
 //dimension 2
 FORWARD_CALL_GF_2(bool,do_intersect,Segment_2,Segment_2)
@@ -126,13 +125,13 @@ FORWARD_CALL_GF_2(CGAL_Object,intersection,Segment_3,Plane_3)
 FORWARD_CALL_GF_2(CGAL_Object,intersection,Segment_3,Segment_3)
 FORWARD_CALL_GF_2(CGAL_Object,intersection,Segment_3,Triangle_3)
 FORWARD_CALL_GF_2(CGAL_Object,intersection,Segment_3,Line_3)
-//FORWARD_CALL_GF_2(CGAL_Object,intersection,Segment_3,Ray_3)
+FORWARD_CALL_GF_2(CGAL_Object,intersection,Segment_3,Ray_3)
 
 FORWARD_CALL_GF_2(CGAL_Object,intersection,Line_3,Plane_3)
 FORWARD_CALL_GF_2(CGAL_Object,intersection,Line_3,Segment_3)
 FORWARD_CALL_GF_2(CGAL_Object,intersection,Line_3,Triangle_3)
 FORWARD_CALL_GF_2(CGAL_Object,intersection,Line_3,Line_3)
-//FORWARD_CALL_GF_2(CGAL_Object,intersection,Line_3,Ray_3)
+FORWARD_CALL_GF_2(CGAL_Object,intersection,Line_3,Ray_3)
 
 FORWARD_CALL_GF_2(CGAL_Object,intersection,Triangle_3,Plane_3)
 FORWARD_CALL_GF_2(CGAL_Object,intersection,Triangle_3,Segment_3)
@@ -141,10 +140,10 @@ FORWARD_CALL_GF_2(CGAL_Object,intersection,Triangle_3,Triangle_3)
 FORWARD_CALL_GF_2(CGAL_Object,intersection,Triangle_3,Ray_3)
 
 FORWARD_CALL_GF_2(CGAL_Object,intersection,Ray_3,Plane_3)
-//FORWARD_CALL_GF_2(CGAL_Object,intersection,Ray_3,Segment_3)
-//FORWARD_CALL_GF_2(CGAL_Object,intersection,Ray_3,Line_3)
+FORWARD_CALL_GF_2(CGAL_Object,intersection,Ray_3,Segment_3)
+FORWARD_CALL_GF_2(CGAL_Object,intersection,Ray_3,Line_3)
 FORWARD_CALL_GF_2(CGAL_Object,intersection,Ray_3,Triangle_3)
-//FORWARD_CALL_GF_2(CGAL_Object,intersection,Ray_3,Ray_3)
+FORWARD_CALL_GF_2(CGAL_Object,intersection,Ray_3,Ray_3)
 
 //dimension 2
 FORWARD_CALL_GF_2(CGAL_Object,intersection,Segment_2,Segment_2)
