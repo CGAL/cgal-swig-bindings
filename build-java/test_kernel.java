5a083f146058e0015a5e10ded182fe79dc55ddfc
Sébastien Loriot <sloriot.ml@gmail.com>
2010-11-23 16:47:15 +0000

    add test file to test polygon
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/cgal-bindings/trunk@318 75acb805-9abe-4c10-b376-15f38c722ce8

---
+++ build-java/test_kernel.java
@@ -0,0 +1,32 @@
+import CGAL.Kernel.Point_2;
+import CGAL.Kernel.Segment_2;
+import CGAL.Kernel.Polygon_2;
+import java.util.LinkedList;
+
+
+public class test_kernel {
+  public static void main(String arg[]){
+    System.loadLibrary("CGAL_Kernel");
+
+    LinkedList<Point_2> ptlst=new LinkedList<Point_2>();
+    ptlst.add( new Point_2(0,0) );
+    ptlst.add( new Point_2(0,1) );
+    ptlst.add( new Point_2(1,1) );
+    
+    Polygon_2 poly=new Polygon_2(ptlst.iterator());
+    
+    System.out.println("poly.size() "+poly.size());
+    
+    for ( Point_2 p : poly.vertices() )
+      System.out.println(p);
+
+    for ( Segment_2 p : poly.edges() )
+      System.out.println(p);
+    
+    
+    
+  }
+
+
+}
+
