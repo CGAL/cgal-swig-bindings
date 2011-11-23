54a48bf6e7f38ec16649dbf369a08a326b08bf55
Sébastien Loriot <sloriot.ml@gmail.com>
2011-11-23 11:01:51 +0000

    remove a bug in spatial searching when the tree is garbage collected but the search class is still in use
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/cgal-bindings/trunk@376 75acb805-9abe-4c10-b376-15f38c722ce8

--- SWIG_CGAL/Spatial_searching/Kd_tree.h
+++ SWIG_CGAL/Spatial_searching/Kd_tree.h
@@ -6,7 +6,7 @@
 #include <SWIG_CGAL/Common/Input_iterator.h>
 #include <SWIG_CGAL/Common/Input_iterator_wrapper.h>
 #include <SWIG_CGAL/Common/Output_iterator_wrapper.h>
-
+#include <boost/shared_ptr.hpp>
 
 SWIG_CGAL_DECLARE_ITERATOR_CLASS_2(iterator,Kd_tree_iterator)
 
@@ -19,12 +19,14 @@ struct Query_iterator_helper{
 
 template<class Cpp_base, class Query, class Fuzzy_sphere, class Fuzzy_iso_box>
 class Kd_tree_wrapper{
-  Cpp_base data;
+  boost::shared_ptr<Cpp_base> data_ptr;
+  Kd_tree_wrapper(const Kd_tree_wrapper<Cpp_base,Query,Fuzzy_sphere,Fuzzy_iso_box>&); //right now CGAL's KDtree does not have a copy constructor.
 public:
   #ifndef SWIG
   typedef Cpp_base cpp_base;
-  const cpp_base& get_data() const {return data;}
-        cpp_base& get_data()       {return data;}
+  const cpp_base& get_data() const {return *data_ptr;}
+        cpp_base& get_data()       {return *data_ptr;}
+  boost::shared_ptr<cpp_base> shared_ptr() {return data_ptr;}
   #endif
   typedef Kd_tree_iterator<Cpp_base,Query> Iterator;
   typedef Query Point_d;
@@ -32,17 +34,17 @@ public:
   typedef typename Query_iterator_helper<Query>::input       Point_range; 
 
 //Creation
-  Kd_tree_wrapper(){}
-  Kd_tree_wrapper(Point_range range):data(range.first,range.second){}
+  Kd_tree_wrapper():data_ptr(new cpp_base()){}
+  Kd_tree_wrapper(Point_range range):data_ptr(new cpp_base(range.first,range.second)){}
   
 //Operations
   SWIG_CGAL_FORWARD_CALL_1(void,insert,Point_d)
-  void insert_range(Point_range range){ data.insert(range.first,range.second);}
-  Iterator iterator(){return Iterator(data.begin(),data.end());}
+  void insert_range(Point_range range){ get_data().insert(range.first,range.second);}
+  Iterator iterator(){return Iterator(get_data().begin(),get_data().end());}
   SWIG_CGAL_FORWARD_CALL_0(void,clear)
   SWIG_CGAL_FORWARD_CALL_0(int,size)
-  void search(typename Query_iterator_helper<Query>::output out, const Fuzzy_sphere& fsphere) { data.search(out,fsphere.get_data());}
-  void search(typename Query_iterator_helper<Query>::output out, const Fuzzy_iso_box& fbox)   { data.search(out,fbox.get_data());}
+  void search(typename Query_iterator_helper<Query>::output out, const Fuzzy_sphere& fsphere) { get_data().search(out,fsphere.get_data());}
+  void search(typename Query_iterator_helper<Query>::output out, const Fuzzy_iso_box& fbox)   { get_data().search(out,fbox.get_data());}
   
   SWIG_CGAL_FORWARD_CALL_0(void,build)
 };
--- SWIG_CGAL/Spatial_searching/NN_search.h
+++ SWIG_CGAL/Spatial_searching/NN_search.h
@@ -3,14 +3,18 @@
 
 #include <SWIG_CGAL/Common/Macros.h>
 #include <SWIG_CGAL/Common/Input_iterator.h>
-
+#include <boost/shared_ptr.hpp>
 
 SWIG_CGAL_DECLARE_ITERATOR_CLASS_2(iterator,NN_search_iterator)
 
 template <class Cpp_base,class Query,class Tree>
 class NN_search_wrapper
 {
+  //use a shared_ptr on the tree to avoid languages with garbage collector to
+  //collect the tree while used by this class  
+  boost::shared_ptr<typename Tree::cpp_base> tree_ptr;
   Cpp_base data;
+  NN_search_wrapper(const NN_search_wrapper<Cpp_base,Query,Tree>&); //prevent copies
 public:
   #ifndef SWIG
   typedef Cpp_base cpp_base;
@@ -20,10 +24,10 @@ public:
   typedef std::pair<Point_d,double>  Point_with_transformed_distance;
   typedef NN_search_iterator<Cpp_base,Point_with_transformed_distance> Iterator;
 //Creation
-  NN_search_wrapper(Tree& tree,const Query& query):data(tree.get_data(),query.get_data()){}
-  NN_search_wrapper(Tree& tree,const Query& query,int k):data(tree.get_data(),query.get_data(),k){}
-  NN_search_wrapper(Tree& tree,const Query& query,int k,double eps):data(tree.get_data(),query.get_data(),k,eps){}
-  NN_search_wrapper(Tree& tree,const Query& query,int k,double eps,bool search_nearest):data(tree.get_data(),query.get_data(),k,eps,search_nearest){}
+  NN_search_wrapper(Tree& tree,const Query& query):tree_ptr(tree.shared_ptr()),data(*tree_ptr,query.get_data()){}
+  NN_search_wrapper(Tree& tree,const Query& query,int k):tree_ptr(tree.shared_ptr()),data(*tree_ptr,query.get_data(),k){}
+  NN_search_wrapper(Tree& tree,const Query& query,int k,double eps):tree_ptr(tree.shared_ptr()),data(*tree_ptr,query.get_data(),k,eps){}
+  NN_search_wrapper(Tree& tree,const Query& query,int k,double eps,bool search_nearest):tree_ptr(tree.shared_ptr()),data(*tree_ptr,query.get_data(),k,eps,search_nearest){}
 //Operations
   Iterator iterator() {
     return Iterator(data.begin(),data.end());
@@ -33,7 +37,11 @@ public:
 template <class Cpp_base,class Query,class Tree>
 class NN_search_wrapper_incremental
 {
+  //use a shared_ptr on the tree to avoid languages with garbage collector to
+  //collect the tree while used by this class
+  boost::shared_ptr<typename Tree::cpp_base> tree_ptr;  
   Cpp_base data;
+  NN_search_wrapper_incremental(const NN_search_wrapper_incremental<Cpp_base,Query,Tree>&); //prevent copies
 public:
   #ifndef SWIG
   typedef Cpp_base cpp_base;
@@ -42,9 +50,9 @@ public:
   typedef std::pair<Point_d,double>  Point_with_transformed_distance;
   typedef NN_search_iterator<Cpp_base,Point_with_transformed_distance> Iterator;
 //Creation
-  NN_search_wrapper_incremental(Tree& tree,const Query& query):data(tree.get_data(),query.get_data()){}
-  NN_search_wrapper_incremental(Tree& tree,const Query& query,double eps):data(tree.get_data(),query.get_data(),eps){}
-  NN_search_wrapper_incremental(Tree& tree,const Query& query,double eps,bool search_nearest):data(tree.get_data(),query.get_data(),eps,search_nearest){}
+  NN_search_wrapper_incremental(Tree& tree,const Query& query):tree_ptr(tree.shared_ptr()),data(*tree_ptr,query.get_data()){}
+  NN_search_wrapper_incremental(Tree& tree,const Query& query,double eps):tree_ptr(tree.shared_ptr()),data(*tree_ptr,query.get_data(),eps){}
+  NN_search_wrapper_incremental(Tree& tree,const Query& query,double eps,bool search_nearest):tree_ptr(tree.shared_ptr()),data(*tree_ptr,query.get_data(),eps,search_nearest){}
 //Operations
   Iterator iterator() {
     return Iterator(data.begin(),data.end());
