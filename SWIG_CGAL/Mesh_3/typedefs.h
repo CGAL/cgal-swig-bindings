a819932e8ec1294f57789cf6b178dfe6fc77b12d
Sébastien Loriot <sloriot.ml@gmail.com>
2011-12-15 11:13:28 +0000

    Finally use Robust_itersection_traits !
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/cgal-bindings/trunk@420 75acb805-9abe-4c10-b376-15f38c722ce8

--- SWIG_CGAL/Mesh_3/typedefs.h
+++ SWIG_CGAL/Mesh_3/typedefs.h
@@ -20,16 +20,46 @@
 
 #include <SWIG_CGAL/Polyhedron_3/all_includes.h>
 
-// Domain
-#warning tmp
-//typedef CGAL::Mesh_3::Robust_intersection_traits_3<EPIC_Kernel> GT;
-//typedef CGAL::Polyhedron_3<GT>                                  Polyhedron;
-typedef CGAL::Polyhedral_mesh_domain_3<Polyhedron_3_, EPIC_Kernel>   PMD;
-typedef CGAL::Mesh_triangulation_3<PMD>::type                        MT_PMD;
-typedef CGAL::Mesh_complex_3_in_triangulation_3<MT_PMD>              C3T3_PMD;
+//Triangle accessor allowing having Polyhedron with a
+//different Kernel
+template < class Polyhedron,class Poly_Kernel, class GT>
+class SWIG_CGAL_Triangle_accessor_3
+{
+public:
+  typedef typename GT::Triangle_3                    Triangle_3;
+  typedef typename Polyhedron::Facet_const_iterator  Triangle_iterator;
+  typedef typename Polyhedron::Facet_const_handle    Triangle_handle;
 
+  SWIG_CGAL_Triangle_accessor_3() { }
+
+  Triangle_iterator triangles_begin(const Polyhedron& p) const
+  {
+    return p.facets_begin();
+  }
+
+  Triangle_iterator triangles_end(const Polyhedron& p) const
+  {
+    return p.facets_end();
+  }
+
+  Triangle_3 triangle(const Triangle_handle& handle) const
+  {
+    typedef typename Poly_Kernel::Point_3 Point;
+    const Point& a = handle->halfedge()->vertex()->point();
+    const Point& b = handle->halfedge()->next()->vertex()->point();
+    const Point& c = handle->halfedge()->next()->next()->vertex()->point();
+    return Triangle_3(a,b,c);
+  }
+};
+
+// Domain
+typedef CGAL::Mesh_3::Robust_intersection_traits_3<EPIC_Kernel>           RIT3;
+typedef SWIG_CGAL_Triangle_accessor_3<Polyhedron_3_,EPIC_Kernel,RIT3>     SGTA3;
+typedef CGAL::Polyhedral_mesh_domain_3<Polyhedron_3_, RIT3,SGTA3>         PMD;
+typedef CGAL::Mesh_triangulation_3<PMD>::type                             MT_PMD;
+typedef CGAL::Mesh_complex_3_in_triangulation_3<MT_PMD>                   C3T3_PMD;
 // Criteria
-typedef CGAL::Mesh_criteria_3<MT_PMD>                                DMC;
+typedef CGAL::Mesh_criteria_3<MT_PMD>                                     DMC;
 
 
 #endif //SWIG_CGAL_MESH_3_TYPEDEFS_H
