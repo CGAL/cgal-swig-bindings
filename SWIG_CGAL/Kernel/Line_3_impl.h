2ea26306016733ca68f71d838ad5fef9102faa57
Sébastien Loriot <sloriot.ml@gmail.com>
2012-10-01 10:31:56 +0200

    using wrong macro

--- SWIG_CGAL/Kernel/Line_3_impl.h
+++ SWIG_CGAL/Kernel/Line_3_impl.h
@@ -12,7 +12,7 @@ Line_3::Line_3(const Point_3& p, Direction_3& d):data(p.get_data(),d.get_data())
 Line_3::Line_3(const Point_3& p, Vector_3& v):data(p.get_data(),v.get_data()){}
 Line_3::Line_3(const Segment_3& s):data(s.get_data()){}
 Line_3::Line_3(const Ray_3& r):data(r.get_data()){}
-SWIG_CGAL_FORWARD_CALL_AND_REF_1(Plane_3,Line_3::perpendicular_plane,Point_3)
+SWIG_CGAL_FORWARD_CALL_AND_REF_SCOPE_1(Plane_3,Line_3::perpendicular_plane,perpendicular_plane,Point_3)
 SWIG_CGAL_FORWARD_CALL_AND_REF_SCOPE_0(Line_3,Line_3::opposite,opposite)
 SWIG_CGAL_FORWARD_CALL_AND_REF_SCOPE_0(Vector_3,Line_3::to_vector,to_vector)
 SWIG_CGAL_FORWARD_CALL_AND_REF_SCOPE_0(Direction_3,Line_3::direction,direction)
