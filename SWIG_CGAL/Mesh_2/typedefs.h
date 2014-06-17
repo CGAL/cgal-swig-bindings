f36d7c7c7834f7b3605fe25923a7a7c54f1a07ac
Sébastien Loriot <sloriot.ml@gmail.com>
2014-06-17 17:58:45 +0200

    add exact tag

--- SWIG_CGAL/Mesh_2/typedefs.h
+++ SWIG_CGAL/Mesh_2/typedefs.h
@@ -18,7 +18,9 @@
 typedef CGAL::Triangulation_vertex_base_2<EPIC_Kernel>                          M2_Vb;
 typedef CGAL::Delaunay_mesh_face_base_2<EPIC_Kernel>                            M2_Fb;
 typedef CGAL::Triangulation_data_structure_2<M2_Vb, M2_Fb>                      M2_Tds;
-typedef CGAL::Constrained_Delaunay_triangulation_2<EPIC_Kernel, M2_Tds>         M2_CDT;
+typedef CGAL::Constrained_Delaunay_triangulation_2< EPIC_Kernel,
+                                                    M2_Tds,
+                                                    CGAL::Exact_predicates_tag> M2_CDT;
 typedef CGAL::Constrained_triangulation_plus_2<M2_CDT>                          M2_CDT_plus;
 
 typedef CGAL::Delaunay_mesh_size_criteria_2<M2_CDT>                             DM2_C;
