90d318e1e6f5b0bef710c1e5c5f1309660ee9129
sciencectn <sciencectn@gmail.com>
2015-02-23 14:47:43 -0800

    Saving this in case I need to stash all the temp files that come out of cmake Need to swig in cpp mode

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 from setuptools import setup
 from distutils.extension import Extension
 
-kernel = Extension("CGAL_Kernel",sources=["CGAL_Kernel.i"])
+kernel = Extension("CGAL_Kernel",sources=["SWIG_CGAL/Kernel/CGAL_Kernel.i"])
 
 
 setup(
