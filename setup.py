e048658c7d5d2466674682410fbab2ba58d6ae4d
sciencectn <sciencectn@gmail.com>
2015-02-23 15:24:56 -0800

    Added ignore so clean -f works Got SWIG to work, but the bindings go to the wrong place

--- setup.py
+++ setup.py
@@ -3,7 +3,10 @@ import setuptools
 from setuptools import setup
 from distutils.extension import Extension
 
-kernel = Extension("CGAL_Kernel",sources=["SWIG_CGAL/Kernel/CGAL_Kernel.i"])
+kernel = Extension("CGAL_Kernel",
+                   sources=["SWIG_CGAL/Kernel/CGAL_Kernel.i"],
+                   swig_opts=["-c++"],
+                   include_dirs=['./'])
 
 
 setup(
