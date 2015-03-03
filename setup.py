fd5a03db98d98ef9b71713bd70ca5a74520119f9
sciencectn <sciencectn@gmail.com>
2015-03-03 14:38:08 -0800

    Added reset script to easily try again

--- setup.py
+++ setup.py
@@ -2,18 +2,42 @@
 import setuptools
 from setuptools import setup
 from distutils.extension import Extension
+import os
+from os.path import join
 
-kernel = Extension("CGAL_Kernel",
+
+def touch(fname, times=None):
+    with open(fname, 'a'):
+        os.utime(fname, times)
+
+OUT_DIR = 'build-python'
+
+PACKAGE_DIR = os.path.join(OUT_DIR,"CGAL")
+
+#Create the package directory structure if it doesn't already exist
+#The existing Cmake install might already have made it
+if not os.path.exists(PACKAGE_DIR):
+    os.makedirs(PACKAGE_DIR)
+
+initpy = os.path.join(PACKAGE_DIR, "__init__.py")
+if not os.path.exists(initpy):
+    touch(initpy)
+
+
+kernel = Extension("_CGAL_Kernel",
                    sources=["SWIG_CGAL/Kernel/CGAL_Kernel.i"],
                    swig_opts=["-c++"],
                    include_dirs=['./'])
 
 
+
 setup(
     name="cgal-bindings",
     description="Bindings so you can use certain CGAL classes in Python",
-    ext_package = "CGAL",
-    ext_modules = [kernel]
+    packages=['CGAL'],
+    ext_package='CGAL',
+    ext_modules = [kernel],
+    package_dir = {'': 'build-python'}
 )
 
 
