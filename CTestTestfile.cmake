17def5be152e7d2004c82302e7d9042bbd75cf50
Cody Fagley <cfagley@codeus.tech>
2019-10-10 15:53:53 -0600

    Repo Cleanup

--- CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/cody/CodeusTech/Other/TetonSimulation/cgal-swig-bindings/src/cgal-swig-bindings/SWIG_CGAL/User_packages/Shape_detection_3
-# Build directory: /home/cody/CodeusTech/Other/TetonSimulation/cgal-swig-bindings/src/cgal-swig-bindings/SWIG_CGAL/User_packages/Shape_detection_3
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
