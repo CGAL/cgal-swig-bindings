6e42b23508bb8997860a65cbbea88f05a1c1d1f8
Thomas Karl Pietrowski <thopiekar@googlemail.com>
2019-10-14 21:13:45 +0200

    WIP: Shape_Detection_3
    As taken from https://github.com/CodyFagley/Shape_detection_3

---
+++ SWIG_CGAL/Shape_detection_3/CGAL_Shape_detection_3.i
@@ -0,0 +1,32 @@
+/*
+  CGAL_Shape_detection_3.i
+  Teton Simulation
+  Authored on   October 10, 2019
+  Last Modified October 10, 2019
+*/
+
+/*
+  Python Interface for CGAL's Shape Detection Algorithm
+
+  This interface contains functions for detecting 3-dimensional 
+  shapes within an unsorted point cloud (e.g. vertex vector).
+
+  Includes support for:
+      * Efficient RANSAC 
+*/
+
+
+//  Define CGAL Module
+%module CGAL_Shape_detection_3
+
+//  Standard API imports
+%include "SWIG_CGAL/common.i"
+%import  "SWIG_CGAL/Common/Macros.h" //C++ macros that helps wrapping CGAL classes
+
+//  Allow 'void' data type
+Decl_void_type()
+
+%{ 
+#include <SWIG_CGAL/User_packages/Shape_detection_3/all_includes.h> 
+%}
+
---
+++ SWIG_CGAL/Shape_detection_3/Shape_detection_3.h
@@ -0,0 +1,133 @@
+/*
+  Shape_detection_3.h
+  Teton Simulation
+  Authored on   October 10, 2019
+  Last Modified October 10, 2019
+*/
+
+/*
+  Contains C/C++ backend for SWIG_CGAL Interface Module
+*/
+
+#ifndef SWIGCGAL_SHAPE_DETECTION_3_H
+#define SWIGCGAL_SHAPE_DETECTION_3_H
+
+
+//  Shape Detection Library
+#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
+#include <CGAL/IO/read_xyz_points.h>
+#include <CGAL/Point_with_normal_3.h>
+#include <SWIG_CGAL/Common/Iterator.h>
+#include <CGAL/property_map.h>
+
+#include <CGAL/Shape_detection_3.h>
+#include <vector>
+
+using namespace std;
+using namespace CGAL;
+
+
+/*
+  Shape Detection Type Definitions
+*/
+typedef CGAL::Exact_predicates_inexact_constructions_kernel  Kernel;
+typedef std::pair<Kernel::Point_3, Kernel::Vector_3>         Point_with_normal;
+typedef std::vector<Point_with_normal>                       Pwn_vector;
+typedef CGAL::First_of_pair_property_map<Point_with_normal>  Point_map;
+typedef CGAL::Second_of_pair_property_map<Point_with_normal> Normal_map;
+// In Shape_detection_traits the basic types, i.e., Point and Vector types
+// as well as iterator type and property maps, are defined.
+typedef CGAL::Shape_detection_3::Shape_detection_traits
+  <Kernel, Pwn_vector, Point_map, Normal_map>                Traits;
+typedef CGAL::Shape_detection_3::Efficient_RANSAC<Traits>    Efficient_ransac;
+typedef CGAL::Shape_detection_3::Region_growing<Traits>      Region_growing;
+typedef CGAL::Shape_detection_3::Plane<Traits>               Plane;
+
+
+template <class Shape_type>
+class Shape_detection_3_wrapper{
+
+/*
+  Private Variables
+*/
+Efficient_ransac shape_detection;
+
+
+/*
+  Public Functions
+*/
+public:
+
+
+//Type Defintions
+
+  typedef SWIG_CGAL_Iterator<Efficient_ransac::Shape_range, Shape_type>   Shape_iterator;
+
+
+//Creation
+  /*
+    Uninitialized Constructor:
+      Creates a new shape-detector without a data set
+  */
+  Shape_detection_3_wrapper()
+  {
+    shape_detection.template add_shape_factory<Shape_type>();
+  }
+
+  /*
+    Initialized Constructor:
+      Creates a new shape-detector with an initial set of data
+      Detects shapes within the set of data
+  */
+  Shape_detection_3_wrapper(Pwn_vector points) 
+  { 
+    shape_detection.set_input(points);
+    shape_detection.template add_shape_factory<Shape_type>();
+    shape_detection.detect(); 
+  }
+
+//Destruction
+  ~Shape_detection_3_wrapper()
+  {
+    shape_detection.clear();
+  }
+
+
+//Accessors
+
+  /* countShapes():
+        Returns number of unique shapes within the given point set
+  */
+  unsigned int countShapes() {return shape_detection.shapes().end() - shape_detection.shapes().begin(); }
+
+
+
+//Iterators
+
+  /* getShapes():
+        Returns detected shapes sorted by largest to smallest in size
+  */
+  Shape_iterator getShapes() {return shape_detection.shapes().begin();}
+
+//Mutators
+
+  /*  SetPoints():
+        Takes a vector of unsorted points and sets the current shape_detection to that set of points
+  */
+  void setPoints(Pwn_vector p) {shape_detection.set_input(p);}
+
+  /*  detect():
+        Detects shapes using the current set of points
+  */
+  void detect() {shape_detection.detect();}
+
+  /*  clearShapes():
+        Clears data set in shape detection object
+  */
+  void clear() {shape_detection.clear();}
+  
+
+
+};
+
+#endif
