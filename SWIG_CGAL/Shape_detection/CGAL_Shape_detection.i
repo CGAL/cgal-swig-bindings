961e3f753cb7304a095fe9aa018e1aa1bc4c935a
Simon Giraudot <simon.giraudot@geometryfactory.com>
2020-01-23 13:32:38 +0100

    Add shape detection

---
+++ SWIG_CGAL/Shape_detection/CGAL_Shape_detection.i
@@ -0,0 +1,50 @@
+// ------------------------------------------------------------------------------
+// Copyright (c) 2020 GeometryFactory (FRANCE)
+// Distributed under the Boost Software License, Version 1.0. (See accompany-
+// ing file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+// ------------------------------------------------------------------------------ 
+
+
+%module (package="CGAL") CGAL_Shape_detection
+
+%include "SWIG_CGAL/common.i"
+Decl_void_type()
+
+SWIG_CGAL_add_java_loadLibrary(CGAL_Shape_detection)
+SWIG_CGAL_package_common()
+
+%import  "SWIG_CGAL/Common/Macros.h"
+%import  "SWIG_CGAL/Kernel/CGAL_Kernel.i"
+%import  "SWIG_CGAL/Point_set_3/CGAL_Point_set_3.i"
+%include "SWIG_CGAL/Common/Iterator.h"
+
+%pragma(java) jniclassimports=%{
+import CGAL.Kernel.Point_3;
+import CGAL.Kernel.Vector_3;
+import CGAL.Point_set_3.Point_set_3;
+import CGAL.Point_set_3.Point_set_3_Int_map;
+import java.util.Iterator;
+import java.util.Collection;
+%}
+
+%pragma(java) moduleimports=%{
+import CGAL.Kernel.Point_3;
+import CGAL.Kernel.Vector_3;
+import CGAL.Point_set_3.Point_set_3;
+import CGAL.Point_set_3.Point_set_3_Int_map;
+import java.util.Iterator;
+import java.util.Collection;
+%};
+
+%types(Point_3*,Point_3);//needed so that the identifier SWIGTYPE_p_Point_3 is generated
+%types(Vector_3*,Vector_3);//needed so that the identifier SWIGTYPE_p_Vector_3 is generated
+
+%include "SWIG_CGAL/Shape_detection/impl.h"
+
+%{
+  #include <SWIG_CGAL/Shape_detection/impl.h>
+%}
+
+#ifdef SWIG_CGAL_HAS_Shape_detection_USER_PACKAGE
+%include "SWIG_CGAL/User_packages/Shape_detection/extensions.i"
+#endif
---
+++ SWIG_CGAL/Shape_detection/impl.h
@@ -0,0 +1,169 @@
+// ------------------------------------------------------------------------------
+// Copyright (c) 2020 GeometryFactory (FRANCE)
+// Distributed under the Boost Software License, Version 1.0. (See accompany-
+// ing file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+// ------------------------------------------------------------------------------
+
+#ifndef SWIG_CGAL_SHAPE_DETECTION_H
+#define SWIG_CGAL_SHAPE_DETECTION_H
+
+#include <SWIG_CGAL/Kernel/Point_3.h>
+#include <SWIG_CGAL/Kernel/Vector_3.h>
+#include <SWIG_CGAL/Point_set_3/Point_set_3.h>
+
+#include <CGAL/Shape_detection/Efficient_RANSAC.h>
+#include <CGAL/Shape_detection/Region_growing/Region_growing.h>
+#include <CGAL/Shape_detection/Region_growing/Region_growing_on_point_set.h>
+
+#include <boost/function_output_iterator.hpp>
+
+#ifndef SWIG
+template <typename NeighborQuery>
+int
+region_growing_impl (Point_set_3_wrapper<CGAL_PS3> point_set,
+                     typename Point_set_3_wrapper<CGAL_PS3>::Int_map plane_map,
+                     int min_points,
+                     double epsilon,
+                     double normal_treshold,
+                     NeighborQuery& neighbor_query)
+{
+  typedef CGAL::Shape_detection::Point_set::Least_squares_plane_fit_region
+    <EPIC_Kernel, CGAL_PS3, CGAL_PS3::Point_map, CGAL_PS3::Vector_map>
+    Region_type;
+  typedef CGAL::Shape_detection::Region_growing
+    <CGAL_PS3, NeighborQuery, Region_type>
+    Region_growing;
+
+  // Init map for unassigned points to be -1
+  for (int& idx : plane_map.get_data())
+    idx = -1;
+
+  double angle = 180 * std::acos(normal_treshold) / CGAL_PI;
+  Region_type region_type (point_set.get_data(), epsilon, angle, min_points,
+                           point_set.get_data().point_map(), point_set.get_data().normal_map());
+  Region_growing region_growing (point_set.get_data(), neighbor_query, region_type);
+
+  int plane_idx = 0;
+  region_growing.detect
+    (boost::make_function_output_iterator
+     ([&](const std::vector<std::size_t>& region)
+      {
+        for (std::size_t idx : region)
+          plane_map.set(*(point_set.get_data().begin() + idx), plane_idx);
+        ++ plane_idx;
+      }));
+
+  return plane_idx;
+}
+#endif
+
+int
+region_growing (Point_set_3_wrapper<CGAL_PS3> point_set,
+                typename Point_set_3_wrapper<CGAL_PS3>::Int_map plane_map,
+                int min_points = 1,
+                double epsilon = -1,
+                double cluster_epsilon = -1,
+                double normal_treshold = 0.9,
+                int k = 0)
+{
+  if (epsilon == -1 && cluster_epsilon == -1)
+  {
+    CGAL::Bbox_3 bbox = CGAL::bbox_3 (point_set.get_data().points().begin(),
+                                      point_set.get_data().points().end());
+    double bbox_diagonal =
+      CGAL::sqrt((bbox.xmax() - bbox.xmin()) * (bbox.xmax() - bbox.xmin())
+                 + (bbox.ymax() - bbox.ymin()) * (bbox.ymax() - bbox.ymin()) 
+                 + (bbox.zmax() - bbox.zmin()) * (bbox.zmax() - bbox.zmin()));
+
+    if (epsilon == -1)
+      epsilon = 0.01 * bbox_diagonal;
+    if (cluster_epsilon == -1)
+      cluster_epsilon = 0.01 * bbox_diagonal;
+  }
+  
+  if (k == 0)
+  {
+   typedef CGAL::Shape_detection::Point_set::Sphere_neighbor_query
+      <EPIC_Kernel, CGAL_PS3, CGAL_PS3::Point_map>
+      Neighbor_query;
+    
+    Neighbor_query neighbor_query (point_set.get_data(), cluster_epsilon, point_set.get_data().point_map());
+    return region_growing_impl (point_set, plane_map, min_points, epsilon, normal_treshold, neighbor_query);
+  }
+  else
+  {
+    typedef CGAL::Shape_detection::Point_set::K_neighbor_query
+      <EPIC_Kernel, CGAL_PS3, CGAL_PS3::Point_map>
+      Neighbor_query;
+    
+    Neighbor_query neighbor_query (point_set.get_data(), k, point_set.get_data().point_map());
+    return region_growing_impl (point_set, plane_map, min_points, epsilon, normal_treshold, neighbor_query);
+  }
+  return 0;
+}
+
+
+boost::shared_ptr<std::vector<std::string> >
+efficient_RANSAC (Point_set_3_wrapper<CGAL_PS3> point_set,
+                  typename Point_set_3_wrapper<CGAL_PS3>::Int_map shape_map,
+                  int min_points = 1,
+                  double epsilon = -1,
+                  double cluster_epsilon = -1,
+                  double normal_threshold = 0.9,
+                  double probability = 0.01,
+                  bool planes = true,
+                  bool cones = false,
+                  bool cylinders = false,
+                  bool spheres = false,
+                  bool tori = false)
+{
+  typedef CGAL::Shape_detection::Efficient_RANSAC_traits
+    <EPIC_Kernel, CGAL_PS3, CGAL_PS3::Point_map, CGAL_PS3::Vector_map> Traits;
+  typedef CGAL::Shape_detection::Efficient_RANSAC<Traits> Efficient_ransac;
+
+  Efficient_ransac ransac;
+  ransac.set_input (point_set.get_data(),
+                    point_set.get_data().point_map(),
+                    point_set.get_data().normal_map());
+
+  if (planes)
+    ransac.add_shape_factory<CGAL::Shape_detection::Plane<Traits> >();
+  if (cones)
+    ransac.add_shape_factory<CGAL::Shape_detection::Cone<Traits> >();
+  if (cylinders)
+    ransac.add_shape_factory<CGAL::Shape_detection::Cylinder<Traits> >();
+  if (spheres)
+    ransac.add_shape_factory<CGAL::Shape_detection::Sphere<Traits> >();
+  if (tori)
+    ransac.add_shape_factory<CGAL::Shape_detection::Torus<Traits> >();
+
+  // Detection
+  Efficient_ransac::Parameters parameters;
+  parameters.probability = probability;
+  parameters.min_points = min_points;
+  parameters.epsilon = epsilon;
+  parameters.cluster_epsilon = cluster_epsilon;
+  parameters.normal_threshold = normal_threshold;
+  ransac.detect (parameters);
+
+  // Init map for unassigned points to be -1
+  for (int& idx : shape_map.get_data())
+    idx = -1;
+
+  // Fill shape map
+  boost::shared_ptr<std::vector<std::string> > output
+    (new std::vector<std::string>());
+  
+
+  for (boost::shared_ptr<Efficient_ransac::Shape> shape : ransac.shapes())
+  {
+    for (std::size_t idx : shape->indices_of_assigned_points())
+      shape_map.set(*(point_set.get_data().begin() + idx), int(output->size()));
+    output->push_back (shape->info());
+  }
+
+  return output;
+}
+
+
+#endif // SWIG_CGAL_SHAPE_DETECTION_H
