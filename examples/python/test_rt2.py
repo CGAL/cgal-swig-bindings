cc7cc09445bbba433d7155ff39927c61af436814
Thomas Karl Pietrowski <thopiekar@googlemail.com>
2020-02-02 20:03:41 +0100

    examples: Python: test_rt2: Making it PEP8 compatible
    Signed-off-by: Thomas Karl Pietrowski <thopiekar@googlemail.com>

--- examples/python/test_rt2.py
+++ examples/python/test_rt2.py
@@ -3,44 +3,44 @@ from CGAL.CGAL_Kernel import Point_3
 from CGAL.CGAL_Kernel import Weighted_point_3
 from CGAL.CGAL_Triangulation_3 import Regular_triangulation_3
 
-t = Regular_triangulation_3()
-l = []
-l.append(Weighted_point_3(Point_3(1, 1, 1), 34))
-l.append(Weighted_point_3(Point_3(1, 56, 2), 334))
-l.append(Weighted_point_3(Point_3(1, 3, 1), 3))
-l.append(Weighted_point_3(Point_3(1, 1, 33), 4))
-t.insert(l)
+triangulation = Regular_triangulation_3()
+list_of_weighted_points = []
+list_of_weighted_points.append(Weighted_point_3(Point_3(1, 1, 1), 34))
+list_of_weighted_points.append(Weighted_point_3(Point_3(1, 56, 2), 334))
+list_of_weighted_points.append(Weighted_point_3(Point_3(1, 3, 1), 3))
+list_of_weighted_points.append(Weighted_point_3(Point_3(1, 1, 33), 4))
+triangulation.insert(l)
 print("OK")
-l.append(1)
+list_of_weighted_points.append(1)
 try:
-    t.insert(l)
-except:
+    triangulation.insert(l)
+except Exception:
     print("list does not contains only points")
 try:
-    t.insert(3)
-except:
+    triangulation.insert(3)
+except Exception:
     print("Not a list")
 
 all_adjacent_vertices = []
 v = 0
 for p in t.finite_vertices():
-    t.finite_adjacent_vertices(p, all_adjacent_vertices)
+    triangulation.finite_adjacent_vertices(p, all_adjacent_vertices)
     v = p
     print(p.point())
 
 print("length of all_adjacent_vertices ", len(all_adjacent_vertices))
 try:
-    t.adjacent_vertices(v, 3)
-except:
+    triangulation.adjacent_vertices(v, 3)
+except Exception:
     print("Not a list")
 
 # test for memory leak: infinite loop
 mylist = []
 i = 5000
 while i != 0:
-    t.adjacent_vertices(v, mylist)
-    t.incident_edges(v, mylist)
-    t.incident_facets(v, mylist)
+    triangulation.adjacent_vertices(v, mylist)
+    triangulation.incident_edges(v, mylist)
+    triangulation.incident_facets(v, mylist)
     print(i, end='')
     mylist = []
     i -= 1
