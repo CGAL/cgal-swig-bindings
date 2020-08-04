faa3c766944728141e2cb8b8e43c8a14defbd362
Maxime Gimeno <maxime.gimeno@gmail.com>
2020-08-04 13:18:26 +0200

    change username

--- .integration/travis.sh
+++ .integration/travis.sh
@@ -9,7 +9,7 @@ if [ $(python -c "import sys; print(sys.version_info.major)") = "3" ]  && [ "$TR
   cd examples/python
   for test_file in ./*.py; do python $test_file; done
   cd ../..
-  twine upload -r testpypi wheelhouse/*  -u "maxgimeno" -p "$CGALTestPyPiPassword" --non-interactive --skip-existing
+  twine upload -r testpypi wheelhouse/*  -u "cgal" -p "$CGALTestPyPiPassword" --non-interactive --skip-existing
 
   cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=~/.local/ -DBUILD_PYTHON=OFF .
   make -j2 VERBOSE=1 && make install -j2 && make tests -j2 VERBOSE=1 && ctest -j2 --output-on-failure
@@ -20,7 +20,7 @@ elif [ $(python -c "import sys; print(sys.version_info.major)") = "3" ]  && [ "$
   cd ../..
   cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=~/.local/ -DBUILD_PYTHON=OFF .
   make -j2 VERBOSE=1 && make install -j2 && make tests -j2 VERBOSE=1 && ctest -j2 --output-on-failure
-  twine upload -r testpypi fixed_wheel/*  -u "maxgimeno" -p "$CGALTestPyPiPassword" --non-interactive --skip-existing
+  twine upload -r testpypi fixed_wheel/*  -u "cgal" -p "$CGALTestPyPiPassword" --non-interactive --skip-existing
 else
   cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=~/.local/ -DPython_FIND_VERSION_MAJOR=2 .
   make -j2 VERBOSE=1 && make install -j2 && make tests -j2 VERBOSE=1 && ctest -j2 --output-on-failure
