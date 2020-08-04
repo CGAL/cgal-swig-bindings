ef0cb8f21c8c03ef91a3b02584ca6d2418407156
Maxime Gimeno <maxime.gimeno@gmail.com>
2020-08-04 16:25:29 +0200

    Don't upload in tests from PRs

--- .integration/travis.sh
+++ .integration/travis.sh
@@ -9,8 +9,10 @@ if [ $(python -c "import sys; print(sys.version_info.major)") = "3" ]  && [ "$TR
   cd examples/python
   for test_file in ./*.py; do python $test_file; done
   cd ../..
-  twine upload -r testpypi wheelhouse/*  -u "cgal" -p "$CGALTestPyPiPassword" --non-interactive --skip-existing
-
+  if [ -n "$DO_UPLOAD" ]; then
+    twine upload -r testpypi wheelhouse/*  -u "cgal" -p "$CGALTestPyPiPassword" --non-interactive --skip-existing
+  fi
+  
   cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=~/.local/ -DBUILD_PYTHON=OFF .
   make -j2 VERBOSE=1 && make install -j2 && make tests -j2 VERBOSE=1 && ctest -j2 --output-on-failure
 elif [ $(python -c "import sys; print(sys.version_info.major)") = "3" ]  && [ "$TRAVIS_OS_NAME" = "osx" ]; then
@@ -20,7 +22,9 @@ elif [ $(python -c "import sys; print(sys.version_info.major)") = "3" ]  && [ "$
   cd ../..
   cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=~/.local/ -DBUILD_PYTHON=OFF .
   make -j2 VERBOSE=1 && make install -j2 && make tests -j2 VERBOSE=1 && ctest -j2 --output-on-failure
-  twine upload -r testpypi fixed_wheel/*  -u "cgal" -p "$CGALTestPyPiPassword" --non-interactive --skip-existing
+  if [ -n "$DO_UPLOAD" ]; then
+    twine upload -r testpypi fixed_wheel/*  -u "cgal" -p "$CGALTestPyPiPassword" --non-interactive --skip-existing
+  fi
 else
   cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=~/.local/ -DPython_FIND_VERSION_MAJOR=2 .
   make -j2 VERBOSE=1 && make install -j2 && make tests -j2 VERBOSE=1 && ctest -j2 --output-on-failure
