1578582d495ee9e3a8c693a523cae3cde3497bfb
Maxime Gimeno <maxime.gimeno@gmail.com>
2021-01-04 09:17:20 +0100

    1st try with ubuntu.yml

--- .integration/travis.sh
+++ .integration/travis.sh
@@ -4,7 +4,7 @@ set -e
 set -x
 export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64
 pycodestyle --show-source --show-pep8 $PYCODESTYLE_CONVENTIONS $CGAL_PYTHON_EXAMPLES || pycodestyle --statistics $PYCODESTYLE_CONVENTIONS -qq $CGAL_PYTHON_EXAMPLES
-if [ $(python -c "import sys; print(sys.version_info.major)") = "3" ]  && [ "$TRAVIS_OS_NAME" = "linux" ]; then
+if [ $(python -c "import sys; print(sys.version_info.major)") = "3" ]  && [ "$1" = "Linux" ]; then
   python -m pip install --verbose wheelhouse/*
   cd examples/python
   for test_file in ./*.py; do python $test_file; done
@@ -15,7 +15,7 @@ if [ $(python -c "import sys; print(sys.version_info.major)") = "3" ]  && [ "$TR
   
   cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=~/.local/ -DBUILD_PYTHON=OFF .
   make -j2 VERBOSE=1 && make install -j2 && make tests -j2 VERBOSE=1 && ctest -j2 --output-on-failure
-elif [ $(python -c "import sys; print(sys.version_info.major)") = "3" ]  && [ "$TRAVIS_OS_NAME" = "osx" ]; then
+elif [ $(python -c "import sys; print(sys.version_info.major)") = "3" ]  && [ "$1" =  "macOS" ]; then
   python -m pip install --verbose fixed_wheel/* --user
   cd examples/python
   for test_file in ./*.py; do python $test_file; done
