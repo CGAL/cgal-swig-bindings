f7de62be5d08acf7cd7e0cb1a78f2f476df7aa7d
Sébastien Loriot <sebastien.loriot@cgal.org>
2025-09-04 11:01:25 +0200

    fix version

--- SWIG_CGAL/Triangulation_2/Constrained_triangulation_plus_2.h
+++ SWIG_CGAL/Triangulation_2/Constrained_triangulation_plus_2.h
@@ -21,7 +21,7 @@ struct Iterator_helper<std::pair<SWIG_Triangulation_2::CGAL_Vertex_handle<Triang
 
   template <class T1, class T2>
   static std::pair<Vertex_handle,Vertex_handle> convert(const std::pair<T1,T2>& i){
-#if CGAL_VERSION_NR >= 1060100910
+#if CGAL_VERSION_NR >= 1060100900
     return std::pair<Vertex_handle,Vertex_handle>(Vertex_handle(i.first),Vertex_handle(i.second));
 #else
     return std::pair<Vertex_handle,Vertex_handle>(Vertex_handle(i.first.first),Vertex_handle(i.first.second));
