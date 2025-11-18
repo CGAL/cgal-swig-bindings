2da2b231f27821ba38f28947f376f98583bb0ffc
Laurent Rineau <laurent.rineau@cgal.org>
2025-11-18 14:49:56 +0100

    reverse the logic

--- setup.py
+++ setup.py
@@ -47,8 +47,8 @@ setup(
     """,
     long_description_content_type="text/markdown",
     url="https://github.com/CGAL/cgal-swig-bindings",
-    conan_requirements=["cmake/[>=3.12 <4]"] + ([] if os.environ.get(
-        'CGAL_PYTHON_MODULE_VERSION') else ["cgal/[>=6.0 <7.0]"]),
+    conan_requirements=["cmake/[>=3.12 <4]"] + (["cgal/[>=6.0 <7.0]"] if not os.environ.get(
+        'CGAL_PYTHON_MODULE_VERSION') else []),
     packages=['CGAL'] + [f'CGAL.CGAL_{m}' for m in CGAL_MODULES],
     package_dir=(
         {'CGAL': 'SWIG_CGAL/files'} |
